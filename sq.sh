dbe02907ac3d62019186b6eecd7c6547c38bb661
esassone <esassone@gmail.com>
2018-01-22 16:07:30 -0300

    Correccion en variable TOBA_BASE_PASS

--- sq.sh
+++ sq.sh
@@ -25,7 +25,7 @@ fi
 
 if [ "$DOCKER_WAIT_FOR" = pg ]; then
 	#Ahora chequeo que se pueda hacer una conexion (pg_isready similar)
-	${TOBA_DIR}/bin/connection_test $TOBA_BASE_HOST $TOBA_BASE_PORT $TOBA_BASE_USER TOBA_BASE_PASS postgres;
+	${TOBA_DIR}/bin/connection_test $TOBA_BASE_HOST $TOBA_BASE_PORT $TOBA_BASE_USER $TOBA_BASE_PASS postgres;
 fi
 if [ -z "$PARAMETRO_INSTALADOR_CREAR_DB" ]; then
 	export PARAMETRO_INSTALADOR_CREAR_DB=--crear-db
